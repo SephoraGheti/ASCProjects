44bc28fd625b2a5532b58424c3a5e46c295afb50
Gheti Sefora <sefora_gheti@yahoo.com>
2020-10-17 13:09:40 -0700

    Moore's Law

---
+++ Moore'sLaw/Moore'sLaw/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Moore_sLaw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXPLICATIE: IN POZA ATASATA
+
+
+            Console.Write("Puterea de calcul fata de momentul dat:");
+            int x = int.Parse(Console.ReadLine());
+            double Timp = (Math.Log2(x) * 18) / 12;
+            Console.WriteLine($"In {Timp} ani, puterea de calcul va deveni de {x} mai mare, ramanand la acelasi pret.");
+
+
+        }
+    }
+}
